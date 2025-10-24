512481aab8090b12511dbda14102a3256c666a09
Ludwin Martínez <99924129+LudwinM16@users.noreply.github.com>
2025-10-24 00:16:16 -0600

    Define light and dark theme colors in Color.kt
    Added light and dark theme color definitions.

--- DSM/app/src/main/java/sv/edu/controlgastos/ui/theme/Color.kt
+++ DSM/app/src/main/java/sv/edu/controlgastos/ui/theme/Color.kt
@@ -2,10 +2,24 @@ package sv.edu.controlgastos.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light Theme Colors
+val md_theme_light_primary = Color(0xFF006D39)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFF76F8A8)
+val md_theme_light_secondary = Color(0xFF4F6353)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_tertiary = Color(0xFF3A646F)
+val md_theme_light_background = Color(0xFFFCFDF7)
+val md_theme_light_surface = Color(0xFFFCFDF7)
+val md_theme_light_onSurface = Color(0xFF1A1C1A)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Dark Theme Colors
+val md_theme_dark_primary = Color(0xFF57DB8E)
+val md_theme_dark_onPrimary = Color(0xFF00391B)
+val md_theme_dark_primaryContainer = Color(0xFF00522A)
+val md_theme_dark_secondary = Color(0xFFB7CCB8)
+val md_theme_dark_onSecondary = Color(0xFF223527)
+val md_theme_dark_tertiary = Color(0xFFA0CED9)
+val md_theme_dark_background = Color(0xFF1A1C1A)
+val md_theme_dark_surface = Color(0xFF1A1C1A)
+val md_theme_dark_onSurface = Color(0xFFE2E3DE)
